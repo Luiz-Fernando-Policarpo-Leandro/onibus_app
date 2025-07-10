c65fc981381eac968522c1d8de21dc99956b5eb5
Luiz-Fernando-Policarpo-Leandro <fasfernandoal@outlook.com.br>
2025-07-09 21:34:53 -0300

    rubocop adjustment

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,21 +3,20 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context 'validate users' do
     it "it's a valid email and password" do
-      status = Status.create!(name:"active")
+      status = Status.create!(name: "active")
       user = User.new(email: 'test@gmail.com', password: "HJtJ1232@", status_id: status.id)
       expect(user).to be_valid
     end
 
     it "it isn't a valid if thero no email, password or status_id" do
       status = Status.create!(name: 'active')
-      user_email = User.new(password: 'HJtJ1232@',status_id: status.id)
+      user_email = User.new(password: 'HJtJ1232@', status_id: status.id)
       user_password = User.new(email: 'erro@gmail.com', status_id: status.id)
       user_status = User.new(email: 'erro@gmail.com', password: "HJtJ1232@")
 
       expect(user_email).not_to be_valid
       expect(user_password).not_to be_valid
-      expect(user_status).not_to be_valid 
-
+      expect(user_status).not_to be_valid
     end
   end
 end
